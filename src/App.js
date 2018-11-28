37bf2b857194ead1b0a7e3097d6d09d1d62ae72d
anil <anilrawat060@gmail.com>
2018-11-28 23:15:20 +0530

    added author functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Book from './Book'
 import Author from './Author'
 import './index.css';
 class App extends React.Component {
-  
-  constructor(props){
-	  super(props)
-	  
-  }
-  
   
   render() {
     return (
--- src/components/authorDetails.js
+++ src/components/authorDetails.js
@@ -4,23 +4,14 @@ import renderHTML from 'react-render-html';
 
 
 const AuthorDetails =({profileData})=>{
-	var style = {
-		backgroundColor:'#EFF0F1',
-		marginLeft:'12%'
-	};
-
-	var imageStyle= {
-		float:'left', 
-		marginRight:'15px'
-
-	}
-	console.log(profileData);
+	
+	
 	return(
 		<div className="panel panel-primary">
 		  <div className="panel-heading jumbotron"><h3>Author Details:</h3>{profileData.name}</div>
 		  <div className="panel-body">
 				<div className='author-image col-md-6'>
-					{ <img src={profileData.image_url.replace("<![CDATA[", "").replace("]]>", "")}/>
+					{ <img alt={profileData.name} src={profileData.image_url.replace("<![CDATA[", "").replace("]]>", "")}/>
 					}
 				</div>
 				<div className='other-detials'>
@@ -28,7 +19,7 @@ const AuthorDetails =({profileData})=>{
 							<label>Fans & Followers: &nbsp; </label> 
 							{profileData.fans_count} at {profileData.author_followers_count}
 						</div>
-						{ profileData.hometown.trim() !=''?<div><label>Hometown:&nbsp; </label>{profileData.hometown}</div>:''  }
+						{ profileData.hometown.trim() !==''?<div><label>Hometown:&nbsp; </label>{profileData.hometown}</div>:''  }
 						<div>
 							<label>Description: &nbsp; </label> 
 							{renderHTML(profileData.about.replace("<![CDATA[", "").replace("]]>", ""))}
